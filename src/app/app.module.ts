0597f3601aa5c8325cea07839e61ffdd23b393ce
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2022-10-27 18:16:21 +0200

    Lecture 19: end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsComponent } from './products.component';
 import { ProductsService } from './products.service';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, ProductComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductComponent,
+    HomeComponent,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ProductsService],
   bootstrap: [AppComponent],
 })
