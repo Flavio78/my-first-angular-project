565cbedbeac227ee754d178f0dc85aa193b81675
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2022-10-27 15:50:30 +0200

    Lecture 13: end

---
+++ src/app/products.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+})
+export class ProductsComponent {
+  productName = 'A Book';
+  isDisabled = true;
+
+  constructor() {
+    setTimeout(() => {
+      this.isDisabled = false;
+    }, 3000);
+  }
+}
